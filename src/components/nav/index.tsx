4f5dfbd716b128e42867909f012d44aa1b53b9c4
its-Danial <itsmalikdanial@gmail.com>
2024-07-04 21:33:31 +0500

    added loader and user namer and full name

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -13,10 +13,10 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import Loader from "../ui/loader";
 
 export default function Nav() {
-  const { data: session, status } = useSession();
-  console.log("session --->", session);
+  const { data: session, status: sessionStatus } = useSession();
 
   return (
     <nav className="sticky top-0 z-50 flex h-16 w-full items-center justify-between border-b bg-background/95 px-8 backdrop-blur supports-[backdrop-filter]:bg-background/60 md:px-16">
@@ -28,32 +28,44 @@ export default function Nav() {
           height={50}
         />
       </Link>
-      <DropdownMenu>
-        <DropdownMenuTrigger className="cursor-pointer shadow" asChild>
-          <Avatar>
-            <AvatarImage
-              src={session?.user.image}
-              alt={session?.user.firstName}
-            />
-            <AvatarFallback>DN</AvatarFallback>
-          </Avatar>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <Link href="/profile" className="w-full">
-              Profile
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem
-            onClick={() => signOut()}
-            className="cursor-pointer"
-          >
-            Log out
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      {sessionStatus === "loading" ? (
+        <Loader size={24} />
+      ) : (
+        <DropdownMenu>
+          <DropdownMenuTrigger className="cursor-pointer" asChild>
+            <div className="flex space-x-2">
+              <Avatar>
+                <AvatarImage
+                  src={session?.user.image}
+                  alt={session?.user.firstName}
+                />
+                <AvatarFallback>DN</AvatarFallback>
+              </Avatar>
+              <div className="flex flex-col space-y-px text-sm">
+                <span className="font-semibold">{`${session?.user.firstName} ${session?.user.lastName}`}</span>
+                <span className="from-accent-foreground">
+                  {session?.user.username}
+                </span>
+              </div>
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href="/profile" className="w-full">
+                Profile
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() => signOut()}
+              className="cursor-pointer"
+            >
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )}
     </nav>
   );
 }
