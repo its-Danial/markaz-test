1c7969982c3c80e16896635f8cd96284ef80822d
its-Danial <itsmalikdanial@gmail.com>
2024-07-04 13:34:17 +0500

    updated styles

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -19,43 +19,41 @@ export default function Nav() {
   console.log("session --->", session);
 
   return (
-    <header className="sticky top-0 flex h-16 items-center border-b bg-background px-8 md:px-16">
-      <nav className="flex w-full items-center justify-between">
-        <Link href="/">
-          <Image
-            src="/black-logo.png"
-            alt="company logo"
-            width={50}
-            height={50}
-          />
-        </Link>
-        <DropdownMenu>
-          <DropdownMenuTrigger className="cursor-pointer shadow" asChild>
-            <Avatar>
-              <AvatarImage
-                src={session?.user.image}
-                alt={session?.user.firstName}
-              />
-              <AvatarFallback>DN</AvatarFallback>
-            </Avatar>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <Link href="/profile" className="w-full">
-                Profile
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem
-              onClick={() => signOut()}
-              className="cursor-pointer"
-            >
-              Log out
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </nav>
-    </header>
+    <nav className="sticky top-0 z-50 flex h-16 w-full items-center justify-between border-b bg-background/95 px-8 backdrop-blur supports-[backdrop-filter]:bg-background/60 md:px-16">
+      <Link href="/">
+        <Image
+          src="/black-logo.png"
+          alt="company logo"
+          width={50}
+          height={50}
+        />
+      </Link>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="cursor-pointer shadow" asChild>
+          <Avatar>
+            <AvatarImage
+              src={session?.user.image}
+              alt={session?.user.firstName}
+            />
+            <AvatarFallback>DN</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href="/profile" className="w-full">
+              Profile
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => signOut()}
+            className="cursor-pointer"
+          >
+            Log out
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </nav>
   );
 }
