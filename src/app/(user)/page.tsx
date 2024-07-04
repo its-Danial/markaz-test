0f4234ffb3f922dc010aa1bb9b36fdbda32826b8
its-Danial <itsmalikdanial@gmail.com>
2024-07-04 13:37:15 +0500

    added all posts page

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,7 +1,41 @@
-export default function Home() {
+import { PostCard } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import $fetch from "@/lib/fetch";
+import { Post } from "@/types";
+import { Search } from "lucide-react";
+
+export default async function Home() {
+  const posts: Post = await (
+    await $fetch("https://dummyjson.com/posts")
+  ).json();
+
   return (
-    <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-8 md:gap-8 md:p-10 md:px-16">
-      <h1>Hello</h1>
+    <main className="min-h-[calc(100vh_-_theme(spacing.16))] bg-muted/40 p-8 md:gap-8 md:p-10 md:px-16">
+      <header className="mb-6 flex items-center justify-between">
+        <div className="flex flex-col space-y-2">
+          <h1 className="text-3xl font-bold tracking-tight md:text-4xl">
+            All Posts
+          </h1>
+          <p className="text-sm text-gray-600">
+            Browser through all{" "}
+            <span className="font-semibold">{posts.total}</span> posts
+          </p>
+        </div>
+        <div className="relative">
+          <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+          <Input
+            type="search"
+            placeholder="Search posts..."
+            className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+          />
+        </div>
+      </header>
+
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+        {posts.posts.map((post) => (
+          <PostCard key={post.id} {...post} />
+        ))}
+      </div>
     </main>
   );
 }
