b1956726abc902369c8eb3bc80dcfeaaa089d579
its-Danial <itsmalikdanial@gmail.com>
2024-07-04 21:33:57 +0500

    made laoder more customizable

--- src/components/ui/loader.tsx
+++ src/components/ui/loader.tsx
@@ -1,19 +1,17 @@
 import { cn } from "@/lib/utils";
 import { AiOutlineLoading } from "react-icons/ai";
 
-export default function Loader({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+export interface LoaderProps extends React.HTMLAttributes<HTMLDivElement> {
+  size?: number;
+}
+
+export default function Loader({ size, className, ...props }: LoaderProps) {
   return (
     <div
-      className={cn(
-        "flex h-[50vh] w-full items-center justify-center px-4",
-        className,
-      )}
+      className={cn("flex items-center justify-center px-4", className)}
       {...props}
     >
-      <AiOutlineLoading size={50} className="animate-spin" />
+      <AiOutlineLoading size={size ?? 50} className="animate-spin" />
     </div>
   );
 }
